e7c9ac09bfe30da34bb2767bc45bedc3476bc671
akash1518 <akashkiranyadav@gmail.com>
2017-09-29 01:46:14 +0530

    greeter added

--- hello.cpp
+++ hello.cpp
@@ -1,7 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
+void myname(){
+	cout<<"hello Akash"<<endl;
+}
 int main()
 {
 	cout<<"hello this my repo"<<endl;
--- second.cpp
+++ second.cpp
@@ -2,9 +2,12 @@
 using namespace std;
 
 
+int sum(int a,int b){
+	return (a+b);
+}
 int main()
 {
-	
+	cout<<sum(3,4)<<endl;
 	cout<<"this is my second file"<endl;
 }
 
